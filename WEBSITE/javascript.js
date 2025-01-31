68ecf67578af7e6ace10dac0ad02daf37915ac03
Farzan53 <farzanahmed66601@gmail.com>
2025-01-31 11:41:30 +0500

    Project

---
+++ WEBSITE/javascript.js
@@ -0,0 +1,706 @@
+
+
+// VIDEOS AND CARDS DISPLAY NONE 
+
+
+document.getElementById("v1").style.display="none"
+document.getElementById("v1-c").style.display="none"
+
+
+document.getElementById("v2").style.display="none"
+document.getElementById("v2-c").style.display="none"
+
+
+document.getElementById("v3").style.display="none"
+document.getElementById("v3-c").style.display="none"
+
+
+document.getElementById("v4").style.display="none"
+document.getElementById("v4-c").style.display="none"
+
+
+document.getElementById("v5").style.display="none"
+document.getElementById("v5-c").style.display="none"
+
+
+document.getElementById("v6").style.display="none"
+document.getElementById("v6-c").style.display="none"
+
+
+
+// HEADING CARDS 1 
+
+
+document.getElementById('img1').addEventListener('click',function(){
+    document.getElementById("v1").style.display="block"
+    document.getElementById("v1-c").style.display="block"
+    
+    
+    document.getElementById("v2").style.display="none"
+    document.getElementById("v2-c").style.display="none"
+    
+    
+    document.getElementById("v3").style.display="none"
+    document.getElementById("v3-c").style.display="none"
+    
+    
+    document.getElementById("v4").style.display="none"
+    document.getElementById("v4-c").style.display="none"
+    
+    
+    document.getElementById("v5").style.display="none"
+    document.getElementById("v5-c").style.display="none"
+    
+    
+    document.getElementById("v6").style.display="none"
+    document.getElementById("v6-c").style.display="none"
+
+})
+
+
+
+//  HEADING CARDS 2 
+
+
+document.getElementById('img2').addEventListener('click',function(){
+    document.getElementById("v1").style.display="none"
+    document.getElementById("v1-c").style.display="none"
+    
+    
+    document.getElementById("v2").style.display="block"
+    document.getElementById("v2-c").style.display="block"
+    
+    
+    document.getElementById("v3").style.display="none"
+    document.getElementById("v3-c").style.display="none"
+    
+    
+    document.getElementById("v4").style.display="none"
+    document.getElementById("v4-c").style.display="none"
+    
+    
+    document.getElementById("v5").style.display="none"
+    document.getElementById("v5-c").style.display="none"
+    
+    
+    document.getElementById("v6").style.display="none"
+    document.getElementById("v6-c").style.display="none"
+
+})
+
+
+
+//  HEADING CARDS 3
+
+
+document.getElementById('img3').addEventListener('click',function(){
+    document.getElementById("v1").style.display="none"
+    document.getElementById("v1-c").style.display="none"
+    
+    
+    document.getElementById("v2").style.display="none"
+    document.getElementById("v2-c").style.display="none"
+    
+    
+    document.getElementById("v3").style.display="block"
+    document.getElementById("v3-c").style.display="block"
+    
+    
+    document.getElementById("v4").style.display="none"
+    document.getElementById("v4-c").style.display="none"
+    
+    
+    document.getElementById("v5").style.display="none"
+    document.getElementById("v5-c").style.display="none"
+    
+    
+    document.getElementById("v6").style.display="none"
+    document.getElementById("v6-c").style.display="none"
+
+})
+
+
+
+
+//  HEADING CARDS 4
+
+
+document.getElementById('img4').addEventListener('click',function(){
+    document.getElementById("v1").style.display="none"
+    document.getElementById("v1-c").style.display="none"
+    
+    
+    document.getElementById("v2").style.display="none"
+    document.getElementById("v2-c").style.display="none"
+    
+    
+    document.getElementById("v3").style.display="none"
+    document.getElementById("v3-c").style.display="none"
+    
+    
+    document.getElementById("v4").style.display="block"
+    document.getElementById("v4-c").style.display="block"
+    
+    
+    document.getElementById("v5").style.display="none"
+    document.getElementById("v5-c").style.display="none"
+    
+    
+    document.getElementById("v6").style.display="none"
+    document.getElementById("v6-c").style.display="none"
+
+})
+
+
+
+
+//  HEADING CARDS 5
+
+
+document.getElementById('img5').addEventListener('click',function(){
+    document.getElementById("v1").style.display="none"
+    document.getElementById("v1-c").style.display="none"
+    
+    
+    document.getElementById("v2").style.display="none"
+    document.getElementById("v2-c").style.display="none"
+    
+    
+    document.getElementById("v3").style.display="none"
+    document.getElementById("v3-c").style.display="none"
+    
+    
+    document.getElementById("v4").style.display="none"
+    document.getElementById("v4-c").style.display="none"
+    
+    
+    document.getElementById("v5").style.display="block"
+    document.getElementById("v5-c").style.display="block"
+    
+    
+    document.getElementById("v6").style.display="none"
+    document.getElementById("v6-c").style.display="none"
+
+})
+
+
+
+
+//  HEADING CARDS 6
+
+
+document.getElementById('img6').addEventListener('click',function(){
+    document.getElementById("v1").style.display="none"
+    document.getElementById("v1-c").style.display="none"
+    
+    
+    document.getElementById("v2").style.display="none"
+    document.getElementById("v2-c").style.display="none"
+    
+    
+    document.getElementById("v3").style.display="none"
+    document.getElementById("v3-c").style.display="none"
+    
+    
+    document.getElementById("v4").style.display="none"
+    document.getElementById("v4-c").style.display="none"
+    
+    
+    document.getElementById("v5").style.display="none"
+    document.getElementById("v5-c").style.display="none"
+    
+    
+    document.getElementById("v6").style.display="block"
+    document.getElementById("v6-c").style.display="block"
+
+})
+
+
+
+// PAGES 1-6 
+
+document.getElementById("clif-p").style.display="none"
+ document.getElementById("clif-b").addEventListener('click',function(){
+
+    document.getElementById("clif-p").style.display="block"
+ })
+
+
+
+
+
+
+
+ document.getElementById("haw-p").style.display="none"
+ document.getElementById("haw-b").addEventListener('click',function(){
+
+    document.getElementById("haw-p").style.display="block"
+ })
+
+
+
+
+ 
+ document.getElementById("fren-p").style.display="none"
+ document.getElementById("fren-b").addEventListener('click',function(){
+
+    document.getElementById("fren-p").style.display="block"
+ })
+
+
+
+
+
+ document.getElementById("kund-p").style.display="none"
+ document.getElementById("kund-b").addEventListener('click',function(){
+
+    document.getElementById("kund-p").style.display="block"
+ })
+
+
+
+
+
+ document.getElementById("gwa-p").style.display="none"
+ document.getElementById("gwa-b").addEventListener('click',function(){
+
+    document.getElementById("gwa-p").style.display="block"
+ })
+
+
+
+
+
+
+
+ document.getElementById("mub-p").style.display="none"
+ document.getElementById("mub-b").addEventListener('click',function(){
+
+    document.getElementById("mub-p").style.display="block"
+ })
+
+
+
+
+
+
+
+
+
+ document.getElementById("mur-p").style.display="none"
+ document.getElementById("mur-b").addEventListener('click',function(){
+
+    document.getElementById("mur-p").style.display="block"
+ })
+
+
+
+
+
+
+
+ document.getElementById("nat-p").style.display="none"
+ document.getElementById("nat-b").addEventListener('click',function(){
+
+    document.getElementById("nat-p").style.display="block"
+ })
+
+
+
+
+
+
+
+
+
+ document.getElementById("ayu-p").style.display="none"
+ document.getElementById("ayu-b").addEventListener('click',function(){
+
+    document.getElementById("ayu-p").style.display="block"
+ })
+
+
+
+
+
+
+
+
+ document.getElementById("kal-p").style.display="none"
+ document.getElementById("kal-b").addEventListener('click',function(){
+
+    document.getElementById("kal-p").style.display="block"
+ })
+
+
+
+ document.getElementById("zia-p").style.display="none"
+ document.getElementById("zia-b").addEventListener('click',function(){
+
+    document.getElementById("zia-p").style.display="block"
+ })
+
+
+
+
+
+ document.getElementById("hun-p").style.display="none"
+ document.getElementById("hun-b").addEventListener('click',function(){
+
+    document.getElementById("hun-p").style.display="block"
+ })
+
+
+
+
+
+
+
+ document.getElementById("moh-p").style.display="none"
+ document.getElementById("moh-b").addEventListener('click',function(){
+
+    document.getElementById("moh-p").style.display="block"
+ })
+
+
+
+
+
+
+ document.getElementById("sha-p").style.display="none"
+ document.getElementById("sha-b").addEventListener('click',function(){
+
+    document.getElementById("sha-p").style.display="block"
+ })
+
+
+
+
+
+ document.getElementById("bad-p").style.display="none"
+ document.getElementById("bad-b").addEventListener('click',function(){
+
+    document.getElementById("bad-p").style.display="block"
+ })
+
+
+
+
+
+
+ document.getElementById("qua-p").style.display="none"
+ document.getElementById("qua-b").addEventListener('click',function(){
+
+    document.getElementById("qua-p").style.display="block"
+ })
+
+
+
+
+
+
+ document.getElementById("sha-p").style.display="none"
+ document.getElementById("sha-b").addEventListener('click',function(){
+
+    document.getElementById("sha-p").style.display="block"
+ })
+
+
+ document.getElementById("shaa-p").style.display="none"
+ document.getElementById("shaa-b").addEventListener('click',function(){
+
+    document.getElementById("shaa-p").style.display="block"
+ })
+
+
+
+ document.getElementById("min-p").style.display="none"
+ document.getElementById("min-b").addEventListener('click',function(){
+
+    document.getElementById("min-p").style.display="block"
+ })
+
+
+
+
+ document.getElementById("nmm-p").style.display="none"
+ document.getElementById("nmm-b").addEventListener('click',function(){
+
+    document.getElementById("nmm-p").style.display="block"
+ })
+
+
+
+ document.getElementById("paf-p").style.display="none"
+ document.getElementById("paf-b").addEventListener('click',function(){
+
+    document.getElementById("paf-p").style.display="block"
+ })
+
+
+
+
+
+ document.getElementById("ins-p").style.display="none"
+ document.getElementById("ins-b").addEventListener('click',function(){
+
+    document.getElementById("ins-p").style.display="block"
+ })
+
+
+
+
+
+
+ document.getElementById("qel-p").style.display="none"
+ document.getElementById("qel-b").addEventListener('click',function(){
+
+    document.getElementById("qel-p").style.display="block"
+ })
+
+
+
+
+
+
+ document.getElementById("nie-p").style.display="none"
+ document.getElementById("nie-b").addEventListener('click',function(){
+
+    document.getElementById("nie-p").style.display="block"
+ })
+
+
+
+
+
+
+ document.getElementById("sin-p").style.display="none"
+ document.getElementById("sin-b").addEventListener('click',function(){
+
+    document.getElementById("sin-p").style.display="block"
+ })
+
+
+
+
+
+
+ document.getElementById("khz-p").style.display="none"
+ document.getElementById("khz-b").addEventListener('click',function(){
+
+    document.getElementById("khz-p").style.display="block"
+ })
+
+
+
+
+
+
+
+ document.getElementById("jyl-p").style.display="none"
+ document.getElementById("jyl-b").addEventListener('click',function(){
+
+    document.getElementById("jyl-p").style.display="block"
+ })
+
+
+
+
+ document.getElementById("ali-p").style.display="none"
+ document.getElementById("ali-b").addEventListener('click',function(){
+
+    document.getElementById("ali-p").style.display="block"
+ })
+
+
+
+
+
+
+ document.getElementById("fwp-p").style.display="none"
+ document.getElementById("fwp-b").addEventListener('click',function(){
+
+    document.getElementById("fwp-p").style.display="block"
+ })
+
+
+
+
+
+
+ document.getElementById("bha-p").style.display="none"
+ document.getElementById("bha-b").addEventListener('click',function(){
+
+    document.getElementById("bha-p").style.display="block"
+ })
+
+  
+
+
+
+ document.getElementById("jpl-p").style.display="none"
+ document.getElementById("jpl-b").addEventListener('click',function(){
+
+    document.getElementById("jpl-p").style.display="block"
+ })
+
+
+
+
+
+
+ document.getElementById("ubw-p").style.display="none"
+ document.getElementById("ubw-b").addEventListener('click',function(){
+
+    document.getElementById("ubw-p").style.display="block"
+ })
+
+
+
+
+
+
+ document.getElementById("sjw-p").style.display="none"
+ document.getElementById("sjw-b").addEventListener('click',function(){
+
+    document.getElementById("sjw-p").style.display="block"
+ })
+
+
+
+
+
+
+ document.getElementById("jgw-p").style.display="none"
+ document.getElementById("jgw-b").addEventListener('click',function(){
+
+    document.getElementById("jgw-p").style.display="block"
+ })
+
+
+
+
+
+
+ document.getElementById("nrw-p").style.display="none"
+ document.getElementById("nrw-b").addEventListener('click',function(){
+
+    document.getElementById("nrw-p").style.display="block"
+ })
+
+
+
+
+
+
+
+
+ document.getElementById("sgw-p").style.display="none"
+ document.getElementById("sgw-b").addEventListener('click',function(){
+
+    document.getElementById("sgw-p").style.display="block"
+ })
+
+
+
+
+
+ document.getElementById("mvw-p").style.display="none"
+ document.getElementById("mvw-b").addEventListener('click',function(){
+
+    document.getElementById("mvw-p").style.display="block"
+ })
+
+
+
+ document.getElementById("contact-p").style.display="none"
+ document.getElementById("contact-b").addEventListener('click',function(){
+
+    document.getElementById("contact-p").style.display="block"
+ })
+
+
+
+ document.getElementById("about-p").style.display="none"
+ document.getElementById("about-b").addEventListener('click',function(){
+
+    document.getElementById("about-p").style.display="block"
+ })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// CONTACT 
+
+
+
+
+document.querySelector("form").addEventListener("submit", function(event) {
+   event.preventDefault();
+   
+   var name = document.getElementById("nl").value;
+   var lastName = document.getElementById("el").value;
+   var email = document.getElementById("cl").value;
+   var phone = document.getElementById("pl").value;
+   var message = document.getElementById("ac").value;
+   
+   var regexName = /^[A-Za-z]+$/;
+   var regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+   var regexPhone = /^\d{10,15}$/;
+   
+   function validateField(value, regex, errorId, errorMessage) {
+       var errorElement = document.getElementById(errorId);
+       if (!value) {
+           errorElement.textContent = errorMessage + " cannot be empty";
+           errorElement.style.color = "red";
+           return false;
+       } else if (!regex.test(value)) {
+           errorElement.textContent = "Invalid " + errorMessage;
+           errorElement.style.color = "red";
+           return false;
+       } else {
+           errorElement.textContent = errorMessage + " is valid";
+           errorElement.style.color = "green";
+           return true;
+       }
+   }
+   
+   var validName = validateField(name, regexName, "error_n", "First Name");
+   var validLastName = validateField(lastName, regexName, "error_e", "Last Name");
+   var validEmail = validateField(email, regexEmail, "error_c", "Email");
+   var validPhone = validateField(phone, regexPhone, "error_p", "Phone Number");
+   var validMessage = validateField(message, /.+/, "error_l", "Message");
+   
+   if (validName && validLastName && validEmail && validPhone && validMessage) {
+       alert("Form submitted successfully!");
+   }
+});
+
+
+
+
+
+
+
+
+
+// PRODUCT CARDS STARS 
+
+
+
+
+
+
+
+
+
